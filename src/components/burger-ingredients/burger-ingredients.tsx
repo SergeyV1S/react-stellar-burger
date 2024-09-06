185f4a92ec5dae74464910b28eef9b656a475fef
Sergey Yatsuk <v1s.coop@yandex.ru>
2024-09-06 17:03:46 +0500

    add burger ingedient scroll

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,27 +1,67 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import type { TRootReducerState } from "@services/reducers";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
-import { TabMenu } from "../ui";
 import burgerIngredients from "./burger-ingredients.module.css";
 import { BurgerItem } from "./burger-item";
 
-const ingredientTypes = [
-  { name: "Булки", type: "bun" },
-  { name: "Основное", type: "main" },
-  { name: "Булки", type: "sauce" }
-];
-
 export const BurgerIngredients = () => {
+  const [currentTab, setCurrentTab] = useState("bun");
+
   const products = useSelector((store: TRootReducerState) => store.ingrediets.data);
+  const bunsRef = useRef<HTMLDivElement>(null);
+  const mainsRef = useRef<HTMLDivElement>(null);
+  const saucesRef = useRef<HTMLDivElement>(null);
+  const ingredientsRef = useRef<HTMLDivElement>(null);
+
+  const ingredientTypes = [
+    { name: "Булки", type: "bun", ref: bunsRef },
+    { name: "Основное", type: "main", ref: mainsRef },
+    { name: "Соусы", type: "sauce", ref: saucesRef }
+  ];
+
+  const selectTab = (tab: string) => {
+    document.getElementById(tab)?.scrollIntoView({ behavior: "smooth" });
+    setCurrentTab(tab);
+  };
+
+  const checkPosition = () => {
+    if (
+      ingredientsRef.current!.getBoundingClientRect().top - mainsRef.current!.getBoundingClientRect().top >
+      bunsRef.current!.getBoundingClientRect().top - ingredientsRef.current!.getBoundingClientRect().top
+    ) {
+      setCurrentTab("main");
+    }
+    if (bunsRef.current!.getBoundingClientRect().top > 180) {
+      setCurrentTab("bun");
+    }
+    if (saucesRef.current!.getBoundingClientRect().top - ingredientsRef.current!.getBoundingClientRect().top < 400) {
+      setCurrentTab("sauce");
+    }
+  };
 
   return (
     <section className={burgerIngredients.container}>
       <h1 className='text text_type_main-large'>Соберите бургер</h1>
-      <TabMenu />
-      <div className={burgerIngredients.ingredients}>
+      <div className={burgerIngredients.tab_menu_container}>
+        <Tab value='bun' active={currentTab === "bun"} onClick={selectTab}>
+          Булки
+        </Tab>
+        <Tab value='main' active={currentTab === "main"} onClick={selectTab}>
+          Начинки
+        </Tab>
+        <Tab value='sauce' active={currentTab === "sauce"} onClick={selectTab}>
+          Соусы
+        </Tab>
+      </div>
+      <div ref={ingredientsRef} onScroll={checkPosition} className={burgerIngredients.ingredients}>
         {ingredientTypes.map((ingredientType) => (
           <div key={ingredientType.type}>
-            <h2 className='text text_type_main-medium'>{ingredientType.name}</h2>
+            <h2 ref={ingredientType.ref} id={ingredientType.type} className='text text_type_main-medium'>
+              {ingredientType.name}
+            </h2>
             <div className={burgerIngredients.type_block_container}>
               {products &&
                 products.map(
