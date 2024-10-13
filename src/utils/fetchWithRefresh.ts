1a6a565b141acf01ac364879ebf39b25b410d573
Sergey Yatsuk <v1s.coop@yandex.ru>
2024-10-13 19:20:27 +0500

    remove all any types

--- src/utils/fetchWithRefresh.ts
+++ src/utils/fetchWithRefresh.ts
@@ -1,12 +1,16 @@
 import { postRefreshTokenMutation } from "../api/postRefreshTokenMutation";
 import { checkReponse } from "./checkResponse";
 
-export const fetchWithRefresh = async (url: string, options: any) => {
+interface FetchOptions extends RequestInit {
+  headers: Record<string, string>;
+}
+
+export const fetchWithRefresh = async (url: string, options: FetchOptions) => {
   try {
     const res = await fetch(url, options);
     return await checkReponse(res);
-  } catch (err: any) {
-    if (err.message === "jwt expired") {
+  } catch (err) {
+    if (err instanceof Error && err.message === "jwt expired") {
       const refreshData = await postRefreshTokenMutation();
       options.headers.authorization = refreshData.accessToken;
       const res = await fetch(url, options);
