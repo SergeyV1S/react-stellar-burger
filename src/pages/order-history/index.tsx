aaf8c226365bd385b34862e242c65b108d8b255c
Sergey Yatsuk <v1s.coop@yandex.ru>
2024-10-26 01:20:39 +0500

    orders page in the profile done

--- src/pages/order-history/index.tsx
+++ src/pages/order-history/index.tsx
@@ -1,10 +1,9 @@
+import { OrderList } from "@components/order-list";
+
 import orderHistoryStyles from "./order-history.module.css";
 
 export const OrderHistoryPage = () => (
-  <div className={orderHistoryStyles.container}>
-    <p className='text text_type_main-medium'>
-      Дяденька, не заходите сюда пожалуйста, страница будет позже, обещаю &#128517;
-    </p>
-    <p className='text text_type_main-medium mt-6'>Всего вам хорошего!</p>
-  </div>
+  <section className={orderHistoryStyles.container}>
+    <OrderList />
+  </section>
 );
